49d9f149dacd14ebff5226b3cfe91a06bf39bfe6
deoladed <leo.robert.mk@gmail.com>
2019-01-14 10:22:28 +0100

    creation fichiers

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,7 +1,20 @@
 def hello
-
+	puts "Hello!"
 end
 
 def greet(name)
-
+	puts "Hello, #{name}!"
 end
+
+def defname
+	puts "Whats your name?"
+	name = gets.chomp
+end
+
+def perform
+	hello
+	greet("Alice")
+	greet("Bob")
+end
+
+perform
